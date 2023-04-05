067dd40ebe234588e82e0e6dcf628bbc1d772a7b
Guillaume Prou <guillaume.prou@ericsson.com>
2023-04-05 16:09:26 +0100

    improve style

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const List = styled.ul`
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  padding: 3px 0;
+  background-color: white;
+`;
+
 const Item = styled.li`
   list-style: none;
+  padding: 6px 8px;
+  &:not(:last-child) {
+    border-bottom: 1px solid #ccc;
+  }
+`;
+
+const DeleteBtn = styled.button`
+  margin-left: 5px;
+  padding: 2px;
+`;
+
+const EmptyList = styled.li`
+  list-style: none;
+  font-style: italic;
+  padding: 6px 8px;
 `;
 
 const TodoList = ({ todos, deleteTodo}) => (
-  <ul>
+  <List>
     {todos.map((todo) => (
       <Item key={todo.id}>
         <span>{todo.text}</span>
-        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        <DeleteBtn onClick={() => deleteTodo(todo.id)}>Delete</DeleteBtn>
       </Item>
     ))}
-  </ul>
+    {!todos.length && <EmptyList>No Items in the list...</EmptyList>}
+  </List>
 );
  
 export default TodoList;
